d128e0752628ce1b29df428d0fb6c19735c25fe0
brysah <brysahvidal@gmail.com>
2024-05-09 15:13:25 -0300

    feat: add ranking styles and data

--- challenge/src/pages/Home/index.jsx
+++ challenge/src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import logo from '../../assets/imgs/logo.png'
 import { Container } from './styles'
+import barbara from '../../assets/imgs/barbara.png'
 
 export function Home() {
     return (
@@ -8,6 +9,18 @@ export function Home() {
                 <img src={logo} alt="Logo A Fazenda" className="header__logo" />
                 <div className="header__title">
                     <h2>RANKING</h2>
+                </div>
+            </div>
+            <div className="ranking">
+                <div className="ranking__item">
+                    <div className="ranking__item__image">
+                        <img src={barbara} alt="barbara" />
+                        <div className="ranking__item__image__counter">1</div>
+                    </div>
+                    <div className="ranking__item__text">
+                        <h3 className="ranking__item__text__title">Barbara Evans</h3>
+                        <p className="ranking__item__text__description">Modelo e filha de Monique Evans</p>
+                    </div>
                 </div>
             </div>
         </Container>
--- challenge/src/pages/Home/styles.js
+++ challenge/src/pages/Home/styles.js
@@ -5,9 +5,9 @@ export const Container = styled.main`
   width: 100%;
   margin: 0 auto;
 
-  background-color: peachpuff;
   .header {
     position: relative;
+    height: 7rem;
     &__logo {
       position: absolute;
       left: 0;
@@ -22,6 +22,47 @@ export const Container = styled.main`
         font-size: 3rem;
         font-weight: bold;
         text-align: end;
+      }
+    }
+  }
+  .ranking {
+    background-color: #fff;
+    padding: 1.6rem 2.4rem;
+    margin-top: 1.6rem;
+    width: 100%;
+    &__item {
+      display: flex;
+      gap: 2rem;
+      align-items: center;
+      &__image {
+        position: relative;
+        max-width: 6.6rem;
+        &__counter {
+          position: absolute;
+          bottom: -5px;
+          right: 3px;
+          color: gray;
+          height: 2rem;
+          width: 2rem;
+          background-color: #fff;
+          border-radius: 50%;
+          border: 1px solid gray;
+          font-size: 1.5rem;
+          font-weight: bold;
+          text-align: center;
+        }
+      }
+      &__text {
+        &__title {
+          font-size: 1.8rem;
+          color: rgb(186, 54, 56);
+          line-height: 1.2;
+        }
+        &__description {
+          font-size: 1.2rem;
+          color: rgb(144, 144, 144);
+          line-height: 1.2;
+        }
       }
     }
   }
